192b8990b035ebe8378e81a6ef67d9600bcc50aa
Joshua Megnauth <joshuamegnauth54@gmail.com>
2020-08-19 10:36:09 -0400

    I decided that having a "builder" like reqwests would be the right path. I started working towards that but it's slow going...

--- src/pushshift.rs
+++ src/pushshift.rs
@@ -1,39 +1,51 @@
-#[warn(clippy::all)]
 use reqwest::blocking::{Client, ClientBuilder};
 use reqwest::{Error, Result};
 use serde::Deserialize;
+use std::collections::HashMap;
+#[warn(clippy::all)]
+use std::fmt::{Display, Formatter};
 
 #[derive(Clone, Copy, Debug)]
-enum PSEndpoint {
+pub enum PSEndpoint {
     Comment,
     Submission,
-    Subreddit
+    Subreddit,
 }
 
-impl PSEndpoint {
-    fn as_str(&self) -> &'static str {
-        match self {
-            PSEndpoint::Comment => "/comment/search",
-            PSEndpoint::Submission => "/submission/search",
-            PSEndpoint::Subreddit => "/subreddit/search"
-        }
+// Free ToString!!
+impl Display for PSEndpoint {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
+        write!(
+            f,
+            "{}",
+            match self {
+                PSEndpoint::Comment => "/comment/search",
+                PSEndpoint::Submission => "/submission/search",
+                PSEndpoint::Subreddit => "/subreddit/search",
+            }
+        )
     }
 }
 
-struct Pushshift {
+// Builds up a Pushshift object similar to how reqwest builds up Clients/Requests.
+pub struct PushshiftBuilder {
     url: String,
-    params: Vec<(String, String)>
+    params: HashMap<String, Vec<String>>,
 }
 
-impl Pushshift {
-    fn new(endpoint: PSEndpoint) -> Self {
-        Pushshift {
-            url: "https://api.pushshift.io/reddit",
-            params: Vec<String, String>::new()
+impl PushshiftBuilder {
+    pub fn new(endpoint: PSEndpoint) -> Self {
+        PushshiftBuilder {
+            url: "https://api.pushshift.io/reddit".to_string() + &endpoint.to_string(),
+            params: HashMap::new(),
         }
     }
 
-    fn subreddit(&mut self, ) {
-        unimplemented()!;
+    fn subreddit(&mut self) {
+        unimplemented!();
+    }
+
+    fn add_param(&mut self, param: &str, par_options: &[String]) {
+        unimplemented!();
     }
 }
